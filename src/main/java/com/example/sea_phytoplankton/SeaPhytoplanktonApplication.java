0f51a1fca3ea99a91d17d3d8fb23f80c9ac3b523
烂柯歧山 <1479039790@qq.com>
2022-12-08 23:06:15 +0800

    Initial commit

---
+++ src/main/java/com/example/sea_phytoplankton/SeaPhytoplanktonApplication.java
@@ -0,0 +1,13 @@
+package com.example.sea_phytoplankton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaPhytoplanktonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaPhytoplanktonApplication.class, args);
+    }
+
+}
